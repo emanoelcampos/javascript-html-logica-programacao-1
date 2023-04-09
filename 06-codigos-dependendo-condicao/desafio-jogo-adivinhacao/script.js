8acc652cc0fec8dd6e03567dd8ac367201e5d7d6
emanoelcampos <emanoel.cv@gmail.com>
2023-04-09 17:42:28 -0300

    desafio jogo adivinhacao

--- 06-codigos-dependendo-condicao/desafio-jogo-adivinhacao/script.js
+++ 06-codigos-dependendo-condicao/desafio-jogo-adivinhacao/script.js
@@ -1,28 +1,74 @@
-alert("Você quer jogar um jogo? ")
+setTimeout(function(){
+    alert("Você se considera bom em adivinhação? Vamos jogar um jogo!");
+},300); 
+
+function mostra(mensagem) {
+    document.write("<h3>" + mensagem + "</h3>")
+}
+
+function mostraFimJogo(){
+    document.write("<body style='background-image: linear-gradient(#D9AFD9, #97D9E1);'></body>")
+    document.write("<h2 style='text-align:center; padding-top: 300px;'>" + "Uau! Você acertou em " + numeroTentativas+ " tentativa(s), pois eu pensei no " + numeroPensado+ "." + "<br><br><button onClick='window.location.reload()'>Jogar outra vez</button>" + "</h2>" );
+}
+
+function mostraVocePerdeu(){
+    document.write("<body style='background-image: linear-gradient(#D9AFD9, #97D9E1);'></body>")
+    document.write("<h2 style='text-align:center; padding-top: 300px;'>" + "Parece que você não é tão bom em adivinhação, você usou todas suas tentativas, o número era " + numeroPensado + "." + "<br><br><button onClick='window.location.reload()'>Tentar outra vez</button>" + "</h2>")
+}
 
 function sorteia(n) {
     return Math.round(Math.random() * n) +1 //adicianar o número 100
 }
 
-let numeroPensado = sorteia(100);
-
-const numeroTotalTentativas = 10;
-let numeroTentativas = 0;
-
-//alert("Você é bom em adivinhação?")
-
-while(numeroTentativas < numeroTotalTentativas) {
-
-    let chute = parseInt(prompt("Qual número você acha que estou pensando?"));
-
-    if(chute == numeroPensado) {
-        alert("Uau! Você acertou em " + numeroTentativas+ ", pois eu pensei no " + numeroPensado+ ".");
-        break;
-    } else if(numeroTentativas == numeroTotalTentativas) {
-        alert("Parece que você não é bom em adivinhação como pensava! O número era " + numeroPensado + ".");
-    } else if(chute > numeroPensado) {
-            alert("Você errou! Seu chute é maior que o número pensado!");
-    } else {
-            alert("Você errou! Seu chute é menor que o número pensado!");
+function limparTentativa() {
+    let campo = document.getElementById("numeroChute");
+    if(campo.value != ""){
+        campo.value = "";
     }
 }
+
+const numeroMaximoTentativas = 11;
+let numeroTentativas = 1;
+let numeroPensado = sorteia(100);
+let campoChute = document.getElementById("numeroChute");
+
+campoChute.focus();
+
+function verificaChute(){
+
+    let chute = document.getElementById("numeroChute").value;
+    chute = Number(chute);
+    
+    while(numeroTentativas < numeroMaximoTentativas) {
+        if(chute == numeroPensado) {
+            //mostra("Uau! Você acertou em " + numeroTentativas+ " tentativa(s), pois eu pensei no " + numeroPensado+ ".");
+            mostraFimJogo();
+            break;
+        } else {
+            numeroTentativas++;
+            let numeroTentativasRestantes = numeroMaximoTentativas - numeroTentativas;
+            if(numeroTentativasRestantes > 0) {
+                if(chute < numeroPensado) {
+                    alert("Você errou! Estou pensando em um número maior.");
+                    break;
+                } else {
+                    alert("Você errou! Estou pensando em um número menor.");
+                    break;
+                }
+            }
+        }if(numeroTentativas == 11) {
+            mostraVocePerdeu();
+        }
+
+    }
+        limparTentativa();
+        campoChute.focus();
+}
+
+campoChute.addEventListener("keypress", function(cliqueEnter) {
+    if(cliqueEnter.key === "Enter") {
+        cliqueEnter.preventDefault();
+        document.getElementById("botaoChute").click();
+    }
+});
+
